8a2a29122673474baf191812b2326f983e570469
Dmitry Novosyolov <i1710@icloud.com>
2019-01-30 02:20:03 +0200

    Initial Commit

---
+++ 3dEarth/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  3dEarth
+//
+//  Created by Dmitry Novosyolov on 30/01/2019.
+//  Copyright © 2019 Dmitry Novosyolov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
